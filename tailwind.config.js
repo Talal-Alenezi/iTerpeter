e4cfac30fa64485cc1b28fd1b309a32d61b6ed5b
Bodour <50752964+bodouralrashidi@users.noreply.github.com>
2023-01-08 00:25:31 +0300

    kinda  responsive + members images + features content+ need to add summery

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
     colors: {
       purple: '#526DFF',
       yellow: '#F1C232',
-      white : '#FFFFFF'
+      white : '#FFFFFF',
+      grey: '#808080',
     },
     screens: {
       'xs': {'min': '300px', 'max': '900px'},},
